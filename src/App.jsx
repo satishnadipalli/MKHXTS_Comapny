9d49b6c5ad60c23d5e578499b22d22d166c419e5
nathivijay <nathivijay542@gmail.com>
2024-08-01 21:16:13 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-import About from './Pages/About/About'
-=======
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,15 +6,11 @@ import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Header from './Components/Header/Header'
 import Home from './Pages/Home/Home'
 
->>>>>>> 7d3578550f47a04f56520891f1e033de39792896
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-<<<<<<< HEAD
-    <About/>
-=======
     <div className='Main_wraper'>
       <Router>
         {/* <Header/> */}
@@ -27,7 +20,6 @@ function App() {
         </Routes>
       </Router>
     </div>
->>>>>>> 7d3578550f47a04f56520891f1e033de39792896
   )
 }
 
