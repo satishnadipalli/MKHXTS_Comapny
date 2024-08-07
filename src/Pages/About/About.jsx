0c0408cca15c6cada0ee1f8b2f9777e69194674b
nathivijay <nathivijay542@gmail.com>
2024-08-07 08:13:09 +0530

    added the home functionality

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -15,18 +15,22 @@ import { FaCode } from "react-icons/fa6";
 import { PiTargetBold } from "react-icons/pi";
 
 const servicesList = [
-  {id: 1,serviceText: "Evidence Generation", icon: <IoMdSettings size={"100%"} />},
-  {id: 2,serviceText: "Strategic market access", icon: <PiStrategy size={"100%"}/>},
-  {id: 3,serviceText: "GVDs",icon: <GrNotes size={"100%"}/>},
-  {id: 4,serviceText: "Contracting& payer strategy", icon: <FaPeopleGroup size={"100%"}/>},
-  {id: 5,serviceText: "Real World Evidence", icon: <IoGlobeOutline size={"100%"}/>},
-  {id: 6,serviceText: "Value Message Testing", icon: <FaBugSlash size={"100%"}/>},
-  {id: 7,serviceText: "Pricing & reimbursement strategy", icon: <GiPriceTag size={"100%"}/>},
-  {id: 8,serviceText: "Launch sequencing assessment", icon: <MdOutlineAssessment size={"100%"}/>},
-  {id: 9,serviceText: "Analog Case Studies & Analysis", icon: <MdOutlineContentPasteSearch size={"100%"}/>},
-  {id: 10,serviceText: "Advisory Boards / Panels (PRISM)", icon: <WiSmallCraftAdvisory size={"100%"}/>},
-  {id: 11,serviceText: "Proprietary Technologies", icon: <FaCode size={"100%"}/>},
-  {id: 12,serviceText: "Systematic / Targeted Literature Reviews", icon: <PiTargetBold size={"100%"}/>}
+  {id: -3, gridClass: "dummy-1"},
+  {id: -2, gridClass: "dummy-2"},
+  {id: -1, gridClass: "dummy-3"},
+  {id: 1,serviceText: "Evidence Generation", icon: <IoMdSettings size={"100%"} />,gridClass: "grid"},
+  {id: 2,serviceText: "Strategic market access", icon: <PiStrategy size={"100%"}/> ,gridClass: "grid"},
+  {id: 3,serviceText: "GVDs",icon: <GrNotes size={"100%"}/> ,gridClass: "grid"},
+  {id: 4,serviceText: "Contracting& payer strategy", icon: <FaPeopleGroup size={"100%"}/> ,gridClass: "grid"},
+  {id: 5,serviceText: "Real World Evidence", icon: <IoGlobeOutline size={"100%"}/> ,gridClass: "grid"},
+  {id: 6,serviceText: "Value Message Testing", icon: <FaBugSlash size={"100%"}/> ,gridClass: "grid"},
+  {id: 7,serviceText: "Pricing & reimbursement strategy", icon: <GiPriceTag size={"100%"}/> ,gridClass: "grid"},
+  {id: 8,serviceText: "Launch sequencing assessment", icon: <MdOutlineAssessment size={"100%"}/> ,gridClass: "grid"},
+  {id: 9,serviceText: "Analog Case Studies & Analysis", icon: <MdOutlineContentPasteSearch size={"100%"}/> ,gridClass: "grid"},
+  {id: 10,serviceText: "Advisory Boards / Panels (PRISM)", icon: <WiSmallCraftAdvisory size={"100%"}/> ,gridClass: "grid"},
+  {id: 11,serviceText: "Proprietary Technologies", icon: <FaCode size={"100%"}/>, gridClass: "grid"},
+  {id: 0, gridClass: "dummy-4"},
+  {id: 12,serviceText: "Systematic / Targeted Literature Reviews", icon: <PiTargetBold size={"100%"}/>, gridClass: "grid"}
 ]
 
 const developmentPhaseList =[
@@ -175,11 +179,21 @@ const About = () => {
         </div>
       </div>
 
-      {/* <div className='our-services'>
-        <
-      </div> */}
-
       <div className='our-services'>
+        <h1 className='our-services-main-head'>OUR SERVICES</h1>
+        <div className='servies'>
+        <ul className='services-list-new'>{servicesList.map(eachService => (
+            <li className={`service-card-new ${eachService.gridClass}`} key={eachService.id}>
+              <div className='service-img-new'>
+                {eachService.icon}
+              </div>
+              <p className='service-name-new'>{eachService.serviceText}</p>
+            </li>
+          ))}</ul>
+        </div>
+      </div>
+
+      {/* <div className='our-services'>
         <h1 className='our-services-main-head'>OUR SERVICES</h1>
         <div className='servies'>
           <ul className='services-list'>{servicesList.map(eachService => (
@@ -191,7 +205,7 @@ const About = () => {
             </li>
           ))}</ul>
         </div>
-      </div>
+      </div> */}
 
       <div className='development-pahes'>
         <h1 className='development-pahes-main-head'>Development Phase</h1>
@@ -213,9 +227,6 @@ const About = () => {
 
       <div className='ceo-div'>
         <div className='ceo'>
-          <div className='ceo-image'>
-            <img src="https://mktxs.com/wp-content/uploads/2023/03/Untitled.png" alt="CEO" className='ceo-img'/>
-          </div>
           <div className='ceo-about'>
             <h1 className='ceo-main-head'>Sandip Shah</h1>
             <p className='ceo-main-para'>President & CEO</p>
@@ -224,6 +235,9 @@ const About = () => {
             <p className='ceo-text'>Prior to starting the company, Sandip was with the Johnson and Johnson family of companies from 1994-2004, where he developed optimal Price and Reimbursement strategies for the pharmaceutical, medical and surgical sectors and worked with local operating companies globally to implement recommendations. Before his time at Johnson and Johnson, Sandip oversaw inline product pricing and contracting at Glaxo Pharmaceuticals. Sandip has also managed sales/marketing services departments including compensation and forecasting.</p>
             <p className='ceo-text'>He holds an MBA from Duke and a Bachelor’s in Chemical Engineering from Purdue University.</p>
             <p className='ceo-text'>Recognized authority in Global Pricing and Market Access for healthcare technology innovations</p>
+          </div>
+          <div className='ceo-image'>
+            <img src="https://mktxs.com/wp-content/uploads/2023/03/Untitled.png" alt="CEO" className='ceo-img'/>
           </div>
         </div>
       </div>
